ef591247fb62faf96305d7e47ee8237b04166105
nlobachev <nikita.lobachev@fintech-farm.com>
2025-02-08 18:00:42 +0200

    Add miles to kilometers conversion

--- Converter/src/app/Main.java
+++ Converter/src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
 public class Main {
+    private static final double MILES_TO_KM = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("Converter App - Version 1.0");
+
+        double miles = 10;
+        double kilometers = convertMilesToKm(miles);
+        System.out.println(miles + " miles is " + kilometers + " kilometers.");
+    }
+
+    private static double convertMilesToKm(double miles) {
+        return miles * MILES_TO_KM;
     }
 }
